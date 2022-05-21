8003bb951f259da72ea64cf1c69dd939f06b95e0
jean-thomas <jt.acquaviva@gmail.com>
2022-05-21 14:22:59 +0200

    improve definition of CS_FCNT_NAME and CS_FNCT_END

--- EXCBR_CSFS_fnct.h
+++ EXCBR_CSFS_fnct.h
@@ -44,21 +44,39 @@ enum CS_FNCT_ID {
         CS_FNCT_END,		// marker of the last supported function
 };
 
-const char *CS_FNCT_NAME[] = {"CS_COUNT_VOWEL", "CS_COUNT_CONSONANT", "CS_MAX", "CS_MIN", "CS_SUM", "CS_PROD", "CS_LAND" , "CS_LOR", "CS_BAND", "CS_BOR", "CS_MAXLOC", "CS_MINLOC", "CS_AVG" , "CS_FIND_FIRST_OCCURENCE"};
-const char *CS_FNCT_DESC[] ={	"returns the number of char elt being vowel",
-				"Returns the number of char elt being consonant",
- 				"Returns the maximum element.",
-				"Returns the minimum element.",
-				"Sums the elements.",
-				"Multiplies all elements.",
-				"Performs a logical and across the elements.",
-				"Performs a logical or across the elements.",
-				"Performs a bitwise and across the bits of the elements.",
-				"Performs a bitwise or across the bits of the elements.",
-				"Returns the maximum value and the rank of the process that owns it.",
-				"Who knowns?",
-				"Return the average value",
-				"Search function returning the position of the first occurence of a pattern",
+//const char *CS_FNCT_NAME[] = {"CS_COUNT_VOWEL", "CS_COUNT_CONSONANT", "CS_MAX", "CS_MIN", "CS_SUM", "CS_PROD", "CS_LAND" , "CS_LOR", "CS_BAND", "CS_BOR", "CS_MAXLOC", "CS_MINLOC", "CS_AVG" , "CS_FIND_FIRST_OCCURENCE"};
+const char *CS_FNCT_NAME[CS_FNCT_END] = {
+	[CS_COUNT_VOWEL]= "CS_COUNT_VOWEL", 
+        [CS_COUNT_CONSONANT] = "CS_COUNT_CONSONANT", 
+ 	[CS_MAX] 	= "CS_MAX",
+	[CS_MIN] 	= "CS_MIN",
+	[CS_SUM] 	= "CS_SUM",
+	[CS_PROD] 	= "CS_PROD",
+	[CS_LAND] 	= "CS_LAND",
+	[CS_LOR]  	= "CS_LOR",
+	[CS_BAND] 	= "CS_BAND",
+	[CS_BOR]	= "CS_BOR",
+	[CS_MAXLOC] 	= "CS_MAXLOC",
+	[CS_MINLOC] 	= "CS_MINLOC",
+ 	[CS_AVG]	= "CS_AVG",
+        [CS_FIND_FIRST_OCCURENCE] = "CS_FIND_FIRST_OCCURENCE",
+	};
+
+const char *CS_FNCT_DESC[CS_FNCT_END] ={
+	[CS_COUNT_VOWEL] ="returns the number of char elt being vowel",
+	[CS_COUNT_CONSONANT] = "Returns the number of char elt being consonant",
+ 	[CS_MAX]        = "Returns the maximum element.",
+	[CS_MIN]	= "Returns the minimum element.",
+	 [CS_SUM]       = "Sums the elements.",
+	[CS_PROD]       = "Multiplies all elements.",
+	[CS_LAND]       = "Performs a logical and across the elements.",
+	[CS_LOR]        = "Performs a logical or across the elements.",
+	[CS_BAND]       = "Performs a bitwise and across the bits of the elements.",
+	[CS_BOR]        = "Performs a bitwise or across the bits of the elements.",
+	[CS_MAXLOC]     = "Returns the maximum value and the rank of the process that owns it.",
+	[CS_MINLOC]     = "Who knowns?",
+	[CS_AVG]        = "Return the average value",
+	[CS_FIND_FIRST_OCCURENCE] = "Search function returning the position of the first occurence of a pattern",
 };
 
 typedef int cs_ptrc_sz_to_int(void*, size_t);
