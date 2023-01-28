287e891602f2fb0d8904e806942d2e4d32b12cfd
JT Acquaviva <jtacquaviva@ddn.com>
2023-01-28 18:06:49 +0100

    Use file descriptor for active storage

--- CSFS_Python/active_proxy.c
+++ CSFS_Python/active_proxy.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include "EXCBR_common.h"
 
 
-double storage_delegate_op(char *fn, size_t offset, size_t sz, char *op)
+double storage_delegate_op(int fd, size_t offset, size_t sz, char *op)
 {
     struct cs_args_t cs_args;
-    int fd;
     double ret;
 
     // we would like to get the fd from Python
-    fd = open(fn, O_RDONLY);
-    if (fd < 0) {
-        perror("open");
+    if (fcntl (fd, F_GETFD) == -1) 
+        {
+        perror("Corrupted file descriptor");
         exit(EXIT_FAILURE);
-    }
+        }
 
     cs_args.in_bfsz = sz;
     cs_args.offset = offset;
@@ -28,7 +28,6 @@ double storage_delegate_op(char *fn, size_t offset, size_t sz, char *op)
     ret = ioctl(fd, CS_OPT, &cs_args);
     cs_args.type_t = CS_DOUBLE_64;
     ret = (double) cs_args.out_bf.d64;
-    close(fd);
 
     return ret;
 }
